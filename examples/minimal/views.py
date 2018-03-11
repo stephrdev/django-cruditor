07c2bdab605eb00bcc59a5540477819d1339e563
Stephan Jaekel <steph@rdev.info>
2018-03-11 20:04:53 +0100

    Add example for additional breadcrumb items.

--- examples/minimal/views.py
+++ examples/minimal/views.py
@@ -7,3 +7,11 @@ from examples.mixins import ExamplesMixin
 class DemoView(ExamplesMixin, CruditorMixin, TemplateView):
     title = 'Demo view'
     template_name = 'minimal/demo.html'
+
+    def get_breadcrumb(self):
+        return super().get_breadcrumb() + [{
+            'url': '/',
+            'title': 'Additional breadcrumb'
+        }, {
+            'title': 'Disabled item'
+        }]
