79054e7b0755d5dfcf34e5a9c088841962fffe12
Stephan Jaekel <steph@rdev.info>
2018-03-21 17:29:00 +0100

    Custom UIKit build.

--- webpack.config.js
+++ webpack.config.js
@@ -4,54 +4,79 @@ const
 	merge = require('webpack-merge'),
 	path = require('path'),
 
-	ExtractTextPlugin = require('extract-text-webpack-plugin'),
-
-	IN_PRODUCTION = (process.env.NODE_ENV === 'production'),
-	COMMON = {
-		devtool: IN_PRODUCTION ? 'source-map' : 'inline-source-map',
-		watch: !IN_PRODUCTION,
-		stats: {
-			modules: false,
-			children: false
-		}
-	},
-	OUTPUT_SCSS = path.join(__dirname, 'cruditor/static/cruditor/css'),
-	SCSS = {
-		entry: {'cruditor': './static/scss/cruditor.scss'},
-		output: {
-			filename: '[name].css',
-			path: OUTPUT_SCSS
-		},
-		module: {
-			rules: [{
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({use: [
-					{loader: 'css-loader'},
-					{loader: 'postcss-loader',
-						options: {
-							ident: 'postcss',
-							plugins: [autoprefixer, cssnano]
-						}
-					},
-					{loader: 'sass-loader'}
-				]})
-			}]
-		},
-		plugins: [
-			new ExtractTextPlugin({
-				filename: '[name].css',
-				allChunks: true
-			})
-		],
-		resolve: {
-			alias: {
-				uikit: path.join(__dirname, 'node_modules/uikit/src/scss')
-			}
-		}
-	}
+	ExtractTextPlugin = require('extract-text-webpack-plugin')
 ;
 
 
-module.exports = [
-	merge(COMMON, SCSS)
-];
+module.exports = function(env, argv) {
+	const
+		COMMON = {
+			mode: argv.mode || 'development',
+			watch: !argv.mode,
+			stats: {
+				modules: false,
+				children: false
+			}
+		},
+		JAVASCRIPT = {
+			entry: {'cruditor': './static/js/cruditor.js'},
+			output: {
+				filename: '[name].js',
+				path: path.join(__dirname, 'cruditor', 'static', 'cruditor', 'js'),
+				library: 'cruditor',
+				libraryTarget: 'umd'
+			},
+			module: {
+				rules: [{
+					test: /\.svg$/,
+					loader: 'html-loader',
+					options: {minimize: false}
+				}]
+			}
+			// resolve: {
+			// 	modules: [
+			// 		path.resolve(__dirname, 'static', 'js', 'src'),
+			// 		path.resolve(__dirname, 'node_modules')
+			// 	]
+			// }
+		},
+		STYLESHEET = {
+			entry: {'cruditor': './static/scss/cruditor.scss'},
+			output: {
+				filename: '[name].css',
+				path: path.join(__dirname, 'cruditor', 'static', 'cruditor', 'css'),
+			},
+			module: {
+				rules: [{
+					test: /\.scss$/,
+					use: ExtractTextPlugin.extract({use: [
+						{loader: 'css-loader'},
+						{loader: 'postcss-loader',
+							options: {
+								ident: 'postcss',
+								plugins: [autoprefixer, cssnano]
+							}
+						},
+						{loader: 'sass-loader'}
+					]})
+				}]
+			},
+			plugins: [
+				new ExtractTextPlugin({
+					filename: '[name].css',
+					allChunks: true
+				})
+			],
+			resolve: {
+				alias: {
+					uikit: path.join(__dirname, 'node_modules/uikit/src/scss')
+				}
+			}
+		}
+	;
+
+	return [
+		merge(COMMON, JAVASCRIPT),
+		merge(COMMON, STYLESHEET)
+	];
+};
