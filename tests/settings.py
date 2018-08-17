8d2cea7e23407728c6229d7a0b8dbcd22cf66d4f
Stephan Jaekel <steph@rdev.info>
2018-08-17 10:45:06 +0200

    Update testsuite to support Django 2.1.

--- tests/settings.py
+++ tests/settings.py
@@ -19,6 +19,7 @@ DATABASES = {
 
 INSTALLED_APPS = (
     'django.contrib.auth',
+    'django.contrib.admin',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
